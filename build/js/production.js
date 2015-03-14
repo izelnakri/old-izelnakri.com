05e59aaf00891b776c95096ab10d1671ae3ea6c1
Izel Nakri <contact@izelnakri.com>
2015-03-14 19:44:22 +0100

    google web fonts added

--- build/js/production.js
+++ build/js/production.js
@@ -1,3 +1,11 @@
+require(['webfont'], function(WebFont) {
+  WebFont.load({
+      google: {
+        families: ['Lato:400,700:latin']
+      }
+    });
+})
+
 require(['jquery', 'underscore', 'backbone','webfont'], function($, _, Backbone, Webfont) {
   console.log("HELLO WORLD");
 })
